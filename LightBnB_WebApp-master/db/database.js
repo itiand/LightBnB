ea06e374a2f515f53456269cfb2a12844d4c419a
Christian Delos Reyes <christian.dreyes@gmail.com>
2023-06-29 13:11:11 -0400

    refactor getuserwithemail, getuserwithid, and add user - connecting to SQL database

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -17,37 +17,73 @@ const pool = new Pool({
  * @return {Promise<{}>} A promise to the user.
  */
 
-const getUserWithEmail = function (email) {
-  let resolvedUser = null;
-  for (const userId in users) {
-    const user = users[userId];
-    if (user?.email.toLowerCase() === email?.toLowerCase()) {
-      resolvedUser = user;
-    }
-  }
-  return Promise.resolve(resolvedUser);
+const getUserWithEmail = function(email) {
+  const query = `SELECT * 
+                FROM users
+                WHERE email = $1
+                `;
+  const values = [email];
+  return pool.query(query, values)
+    .then(response => {
+      return response.rows[0];
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
+
+  //EXPECTED RESULT
+  // {
+  //   name: 'Estella Rios',
+  //   email: 'elizabethyork@ymail.com',
+  //   password: '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.',
+  //   id: 1
+  // }
 };
+
 
 /**
  * Get a single user from the database given their id.
  * @param {string} id The id of the user.
  * @return {Promise<{}>} A promise to the user.
  */
-const getUserWithId = function (id) {
-  return Promise.resolve(users[id]);
+const getUserWithId = function(id) {
+  const query = `SELECT * 
+  FROM users
+  WHERE users.id = $1
+  `;
+  const values = [id];
+  return pool.query(query, values)
+    .then(response => {
+      return response.rows[0];
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
 };
 
+// getUserWithId(1).then( res => console.log('RESSS', res))
 /**
  * Add a new user to the database.
  * @param {{name: string, password: string, email: string}} user
  * @return {Promise<{}>} A promise to the user.
  */
-const addUser = function (user) {
-  const userId = Object.keys(users).length + 1;
-  user.id = userId;
-  users[userId] = user;
-  return Promise.resolve(user);
-};
+const addUser = function(user) {
+
+  const query = `INSERT INTO users (name, email, password)
+                VALUES ($1, $2, $3)
+                RETURNING *`;
+  const values = [ user.name, user.email, user.password];
+
+  return pool.query(query, values)
+    .then(response => {
+      console.log('ressss', response);
+      return response;
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
+}
+
 
 /// Reservations
 
@@ -56,7 +92,7 @@ const addUser = function (user) {
  * @param {string} guest_id The id of the user.
  * @return {Promise<[{}]>} A promise to the reservations.
  */
-const getAllReservations = function (guest_id, limit = 10) {
+const getAllReservations = function(guest_id, limit = 10) {
   return getAllProperties(null, 2);
 };
 
@@ -68,21 +104,16 @@ const getAllReservations = function (guest_id, limit = 10) {
  * @param {*} limit The number of results to return.
  * @return {Promise<[{}]>}  A promise to the properties.
  */
-const getAllProperties = function (options, limit = 10) {
+const getAllProperties = function(options, limit = 10) {
   return pool.query(`SELECT * FROM properties
     LIMIT $1;`, [limit])
     .then(response => {
-      console.log('RESULT ROWS', result.rows);
+      console.log('RESULT ROWS', response.rows);
       return response.rows;
     })
     .catch((err) => {
       console.log(err.message);
-    })
-  // const limitedProperties = {};
-  // for (let i = 1; i <= limit; i++) {
-  //   limitedProperties[i] = properties[i];
-  // }
-  // return Promise.resolve(limitedProperties);
+    });
 };
 
 /**
@@ -90,7 +121,7 @@ const getAllProperties = function (options, limit = 10) {
  * @param {{}} property An object containing all of the property details.
  * @return {Promise<{}>} A promise to the property.
  */
-const addProperty = function (property) {
+const addProperty = function(property) {
   const propertyId = Object.keys(properties).length + 1;
   property.id = propertyId;
   properties[propertyId] = property;
